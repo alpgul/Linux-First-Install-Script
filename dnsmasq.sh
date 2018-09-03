b2f2b27c5f3939c8185bbf67aa119f56e2beaef2
Alptekin Gülcan <alptekingulcan@hotmail.com>
2018-09-03 18:19:04 +0300

    Update dnsmasq.sh

--- dnsmasq.sh
+++ dnsmasq.sh
@@ -1,5 +1,12 @@
-
-
+#!/bin/bash
+# Dnsmasq Server
+if [ $SUDO_USER ]; 
+then 
+	user=$SUDO_USER;echo 
+else 
+	echo "Must be run as root user!!" 
+	exit 1 
+fi
 sudo apt-get install dnsmasq
 cat <<EOF >> /etc/dnsmasq.conf
 interface=lo,ap0
