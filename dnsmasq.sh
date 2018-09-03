03efdf89b2e8c86ac3cc17e0a8fef6ed5ff075d4
Alptekin Gülcan <alptekingulcan@hotmail.com>
2018-09-03 18:16:02 +0300

    Update dnsmasq.sh

--- dnsmasq.sh
+++ dnsmasq.sh
@@ -2,12 +2,9 @@
 
 sudo apt-get install dnsmasq
 cat <<EOF >> /etc/dnsmasq.conf
-# disables dnsmasq reading any other files like /etc/resolv.conf for nameservers
-no-resolv
-# Interface to bind to
-interface=wlan0
-# Specify starting_range,end_range,lease_time
-dhcp-range=192.168.50.3,192.168.50.20,12h
-# dns addresses to send to the clients
+interface=lo,ap0
+no-dhcp-interface=lo,wlan0
+bind-interfaces
 server=127.0.0.1#40
+dhcp-range=192.168.50.50,192.168.50.150,12h
 EOF
