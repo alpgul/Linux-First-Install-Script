d7576a00e9fe8ae3f5d0914a358508571b49c195
Alptekin Gülcan <alptekingulcan@hotmail.com>
2018-09-03 16:29:53 +0300

    Create Rpi-Monitor.sh

---
+++ Rpi-Monitor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Raspberry Monitor
+cd /tmp
+sudo wget https://goo.gl/yDYFhy -O rpimonitor_latest.deb
+sudo chmod +x rpimonitor_latest.deb
+sudo aptitude install librrds-perl libhttp-daemon-perl libjson-perl \
+libipc-sharelite-perl libfile-which-perl libsnmp-extension-passpersist-perl
+sudo dpkg -i rpimonitor_latest.deb
+sudo /usr/share/rpimonitor/scripts/updatePackagesStatus.pl
+sudo sed -i  "s#\#include=/etc/rpimonitor/template/services.conf#include=/etc/rpimonitor/template/services.conf#g" /etc/rpimonitor/data.conf
+sudo sed -i  "s#\#include=/etc/rpimonitor/template/wlan.conf#include=/etc/rpimonitor/template/wlan.conf#g" /etc/rpimonitor/data.conf
+sudo wget https://raw.githubusercontent.com/alpgul/Linux-First-Install-Script/master/RpiMonitor/Services.conf -O /etc/rpimonitor/template/services.conf
+sudo service rpimonitor restart
+sudo service rpimonitor update
