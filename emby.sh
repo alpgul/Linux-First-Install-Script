51bf76cfcb5ba4a7fb9dbdccc70c2561ad3a58cb
Alptekin Gülcan <alptekingulcan@hotmail.com>
2018-09-03 18:37:21 +0300

    Update emby.sh

--- emby.sh
+++ emby.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Emby Media Server
-
+if [ $SUDO_USER ]; 
+then 
+	user=$SUDO_USER;echo 
+else 
+	echo "Must be run as root user!!" 
+	exit 1 
+fi
 cd /tmp/
 wget https://github.com/MediaBrowser/Emby.Releases/releases/download/3.5.2.0/emby-server-deb_3.5.2.0_armhf.deb
 sudo dpkg -i emby-server-deb_3.5.2.0_armhf.deb
