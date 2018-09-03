aac9e74db58bccfe275b0d9b1e4366fd5ef15ea9
Alptekin Gülcan <alptekingulcan@hotmail.com>
2018-09-03 18:03:49 +0300

    Update qbittorrent.sh

--- qbittorrent.sh
+++ qbittorrent.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Torrent Server
 
+if [ $SUDO_USER ]; 
+then 
+	user=$SUDO_USER;echo 
+else 
+	echo "Must be run as root user!!" 
+	exit 1 
+fi
 sudo apt-get install qbittorrent-nox -y
 cat <<EOF >> /etc/systemd/system/qbittorrent.service
 [Unit]
