88a8cef926b16bc87a5e4ae225e6090256d29c91
Alptekin Gülcan <alptekingulcan@hotmail.com>
2018-09-03 11:17:33 +0300

    Create emby.sh

---
+++ emby.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Emby Media Server
+
+cd /tmp/
+wget https://github.com/MediaBrowser/Emby.Releases/releases/download/3.5.2.0/emby-server-deb_3.5.2.0_armhf.deb
+sudo dpkg -i emby-server-deb_3.5.2.0_armhf.deb
