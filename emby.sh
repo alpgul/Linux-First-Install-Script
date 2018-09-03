768a4caff87125c9c08493b20823a20e375a82aa
Alptekin Gülcan <alptekingulcan@hotmail.com>
2018-09-03 11:41:09 +0300

    Update emby.sh

--- emby.sh
+++ emby.sh
@@ -4,3 +4,4 @@
 cd /tmp/
 wget https://github.com/MediaBrowser/Emby.Releases/releases/download/3.5.2.0/emby-server-deb_3.5.2.0_armhf.deb
 sudo dpkg -i emby-server-deb_3.5.2.0_armhf.deb
+service emby-server restart
