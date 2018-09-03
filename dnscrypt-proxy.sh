acd830603b7044d4796e49c6fac532af3c313e22
Alptekin Gülcan <alptekingulcan@hotmail.com>
2018-09-03 15:41:43 +0300

    Create dnscrypt-proxy.sh

---
+++ dnscrypt-proxy.sh
@@ -0,0 +1,14 @@
+
+cd /tmp
+wget https://github.com/jedisct1/dnscrypt-proxy/releases/download/2.0.16/dnscrypt-proxy-linux_arm-2.0.16.tar.gz
+sudo chmod +x dnscrypt-proxy-linux_arm-2.0.16.tar.gz
+sudo mkdir /opt/dnscrypt-proxy
+sudo tar xzf /tmp/dnscrypt-proxy-linux_arm-2.0.16.tar.gz -C /opt/dnscrypt-proxy
+cd /opt/dnscrypt-proxy/linux-arm
+sudo cp example-dnscrypt-proxy.toml dnscrypt-proxy.toml
+sudo apt-get remove resolvconf -y
+sudo cp /etc/resolv.conf /etc/resolv.conf.backup
+sudo sed -i "s#listen_addresses = \['127.0.0.1:53', '\[::1\]:53'#listen_addresses = ['127.0.0.1:40', '[::1]:40'#" /opt/dnscrypt-proxy/linux-arm/dnscrypt-proxy.toml
+sudo ./dnscrypt-proxy -service install
+sudo ./dnscrypt-proxy -service start
+sudo /etc/init.d/networking restart
